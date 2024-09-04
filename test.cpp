e2eeea8e8b789210f28b4ad1754cfbcc0eb2fcb5
kim214958 <kimgnltmd@naver.com>
2024-09-04 23:02:05 +0900

    test in home

--- test.cpp
+++ test.cpp
@@ -12,5 +12,8 @@ int main(){
 int main(){
     std::cout<<"Hello World"<<std::endl;
     //std::cout<<"__cplusplus: "<<__cplusplus<<std::endl;
+<<<<<<< HEAD
 >>>>>>> 0ce3955 (test in home)
+=======
+>>>>>>> 114ef71 (test in home)
 }
